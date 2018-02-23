61e5ef049b4a4351cfcd51e68a41a66495f9dfe4
biexiande <biexiande@hisense.com>
2018-02-23 13:19:54 +0800

    INIT

--- src/main/java/com/ryan/server/NettyServer.java
+++ src/main/java/com/ryan/server/NettyServer.java
@@ -40,7 +40,7 @@ public class NettyServer {
                     //字符串编码器
                     pipeline.addLast(new StringEncoder());
                     //处理类
-                    //pipeline.addLast(new ServerHandler());
+                    pipeline.addLast(new ServerHandler());
                 }
             });
 
@@ -66,5 +66,38 @@ public class NettyServer {
             boss.shutdownGracefully();
             worker.shutdownGracefully();
         }
+    }
+}
+
+class ServerHandler extends SimpleChannelInboundHandler<String> {
+
+    //读取客户端发送的数据
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+        System.out.println("client response :"+msg);
+        ctx.channel().writeAndFlush("i am server !");
+
+//        ctx.writeAndFlush("i am server !").addListener(ChannelFutureListener.CLOSE);
+    }
+
+    //新客户端接入
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelActive");
+    }
+
+    //客户端断开
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelInactive");
+    }
+
+    //异常
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        //关闭通道
+        ctx.channel().close();
+        //打印异常
+        cause.printStackTrace();
     }
 }
